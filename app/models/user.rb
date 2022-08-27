ec221803b2e38faf61ef94d4ecff3733defb8f0b
Eva Israelian <evais1@mail.ru>
2022-08-27 16:30:25 +0300

    register bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   private
 
   def link_subscriptions
-    Subscription.where(user_id: nil, user_email: user.email).update_all(user_id: self.id)
+    Subscription.where(user_id: nil, user_email: self.email)
+      .update_all(user_id: self.id)
   end
 end
