53532746e1550e1665619c1362277a67a5948ded
Eva Israelian <evais1@mail.ru>
2022-08-28 00:05:51 +0300

    avatars, event photos local version

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,32 @@ module ApplicationHelper
   def bi_icon(icon_class)
     content_tag 'span', '', class: "bi bi-#{icon_class}"
   end
+
+  def event_photo(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.url
+    else
+      asset_path('event.jpg')
+    end
+  end
+
+  def event_thumb(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.thumb.url
+    else
+      asset_path('event_thumb.jpg')
+    end
+  end
+
+  def user_avatar_thumb(user)
+    if user.avatar.file.present?
+      user.avatar.thumb.url
+    else
+      asset_path('avatar.png')
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :subscriptions
   devise_for :users
   root to: 'events#index'
 
   resources :events do
     resources :comments, only: %i[create destroy]
     resources :subscriptions, only: %i[create destroy]
+    resources :photos, only: %i[create destroy]
   end
 
   resources :users, only: %i[show edit update]
