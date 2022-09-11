cc457fe9b9c3cc74811734fe4cce6afa62110dca
Eva Israelian <evais1@mail.ru>
2022-09-11 19:59:17 +0300

    lightbox fix

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -7,7 +7,6 @@ import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'stylesheets/application'
-import 'lightbox2/dist/js/lightbox-plus-jquery'
 
 Rails.start()
 Turbolinks.start()
