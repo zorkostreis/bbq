d388fd8562bace2636c90618313087b4d53410ab
Eva Israelian <evais1@mail.ru>
2022-09-11 11:00:04 +0300

    added lightbox2

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -7,6 +7,7 @@ import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'stylesheets/application'
+import 'lightbox2/dist/js/lightbox-plus-jquery'
 
 Rails.start()
 Turbolinks.start()
