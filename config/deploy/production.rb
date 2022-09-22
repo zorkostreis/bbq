0794d8d89aedf769414ffadb37460ea0203d0c79
Eva Israelian <evais1@mail.ru>
2022-09-22 10:51:14 +0300

    code style fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server 'bbqevent.site', user: 'deploy', roles: %w{app db web}
+server 'bbqevent.site', user: 'deploy', roles: %w[app db web]
 
 # role-based syntax
 # ==================
