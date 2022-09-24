6d63c20672a7cb3f1233a5d84f815739a857d612
Eva Israelian <evais1@mail.ru>
2022-09-24 15:16:14 +0300

    add resque_worker for capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,10 +7,10 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server 'bbqevent.site', user: 'deploy', roles: %w[app db web resque]
+server 'bbqevent.site', user: 'deploy', roles: %w[app db web resque_worker]
 
-# set :resque_environment_task, true
-# set :workers, { "#{fetch(:application)}*" => 1 }
+set :resque_environment_task, true
+set :workers, { "#{fetch(:application)}*" => 1 }
 
 # role-based syntax
 # ==================
